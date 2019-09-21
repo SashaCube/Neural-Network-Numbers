54eb0dfa1e99add38e233aa87874693cb695318c
SashaCube <sashagavgavgav@gmail.com>
2019-09-21 12:29:52 +0300

    add: Network class add: Utils class

--- src/main/kotlin/com/havruliyk/nnnumbers/nn/Network.kt
+++ src/main/kotlin/com/havruliyk/nnnumbers/nn/Network.kt
@@ -1,5 +1,21 @@
 package com.havruliyk.nnnumbers.nn
 
-class Network(){
+class Network(
+        val inputLayer: MutableList<Neuron> = mutableListOf()
+) {
+    init {
+        for (i in 0..NUMBER_OF_NEURONS) {
+            inputLayer.add(Neuron(0F))
+        }
+    }
 
+    private fun respond(card: Card) {
+        inputLayer.mapIndexed { index, neuron ->
+            neuron.output = card.inputs[index]
+        }
+    }
+
+    companion object {
+        const val NUMBER_OF_NEURONS = 192
+    }
 }
---
+++ src/main/kotlin/com/havruliyk/nnnumbers/util/Utils.kt
@@ -0,0 +1,8 @@
+package com.havruliyk.nnnumbers.util
+
+import com.havruliyk.nnnumbers.nn.Card
+
+val TEST_CARDS = {
+    val cardList = mutableListOf<Card>()
+    cardList.add(Card())
+}
